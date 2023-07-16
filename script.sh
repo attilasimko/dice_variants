ff02406e447579480db83cb64e6c614bdf0145a9
Attila <attila.simko@umu.se>
2023-07-16 16:06:21 +0200

    typo

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python training.py --gpu 0 --base gauss --num_epochs 1 --loss dice --learning_Rate 0.0001
-python training.py --gpu 0 --base gauss --num_epochs 1 --loss cross_entropy --learning_Rate 0.0001
-python training.py --gpu 0 --base gauss --num_epochs 1 --loss mime --learning_Rate 0.0001
+python training.py --gpu 0 --base gauss --num_epochs 1 --loss dice --learning_rate 0.0001
+python training.py --gpu 0 --base gauss --num_epochs 1 --loss cross_entropy --learning_rate 0.0001
+python training.py --gpu 0 --base gauss --num_epochs 1 --loss mime --learning_rate 0.0001
