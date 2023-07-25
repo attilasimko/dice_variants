a8a9e8b3cf0698ea55794cf7e33f70b8e47491ac
Attila <attila.simko@umu.se>
2023-07-25 20:03:10 +0200

    go

--- script.sh
+++ script.sh
@@ -1,28 +1,12 @@
 #!/bin/sh
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss dice --learning_rate 0.000001
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss cross_entropy --learning_rate 0.000001
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss mime --learning_rate 0.000001
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss dice --learning_rate 0.00001
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss cross_entropy --learning_rate 0.00001
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss mime --learning_rate 0.00001
 
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss dice --learning_rate 0.000005
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss cross_entropy --learning_rate 0.000005
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss mime --learning_rate 0.000005
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss dice --learning_rate 0.00005
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss cross_entropy --learning_rate 0.00005
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss mime --learning_rate 0.00005
 
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss dice --learning_rate 0.00001
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss cross_entropy --learning_rate 0.00001
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss mime --learning_rate 0.00001
-
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss dice --learning_rate 0.00005
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss cross_entropy --learning_rate 0.00005
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss mime --learning_rate 0.00005
-
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss dice --learning_rate 0.0001
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss cross_entropy --learning_rate 0.0001
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss mime --learning_rate 0.0001
-
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss dice --learning_rate 0.0005
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss cross_entropy --learning_rate 0.0005
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss mime --learning_rate 0.0005
-
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss dice --learning_rate 0.001
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss cross_entropy --learning_rate 0.001
-python training.py --gpu 0 --base gauss --num_epochs 15 --loss mime --learning_rate 0.001
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss dice --learning_rate 0.0001
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss cross_entropy --learning_rate 0.0001
+python training.py --gpu 0 --base gauss --num_epochs 30 --loss mime --learning_rate 0.0001
