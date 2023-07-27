3b14ae3661835ec5f1944b9a52821557b1c30121
Attila <attila.simko@umu.se>
2023-07-27 14:00:46 +0200

    new model

--- model.py
+++ model.py
@@ -124,32 +124,36 @@ def unet_2d(input_shape, num_filters, num_classes, batchnorm=False):
     if (batchnorm):
         x = BatchNormalization()(x)
 
-
-    x = Conv2DTranspose(num_filters * 8, strides=(2, 2), kernel_size=2, padding='same')(x)
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(num_filters * 8, kernel_size=2, padding='same')(x)
     x = Concatenate()([x, x_5])
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = Conv2DTranspose(num_filters * 8, strides=(2, 2), kernel_size=2, padding='same')(x)
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(num_filters * 8, kernel_size=2, padding='same')(x)
     x = Concatenate()([x, x_4])
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = Conv2DTranspose(num_filters * 4, strides=(2, 2), kernel_size=2, padding='same')(x)
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(num_filters * 4, kernel_size=2, padding='same')(x)
     x = Concatenate()([x, x_3])
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = Conv2DTranspose(num_filters * 2, strides=(2, 2), kernel_size=2, padding='same')(x)
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(num_filters * 2, kernel_size=2, padding='same')(x)
     x = Concatenate()([x, x_2])
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = Conv2DTranspose(num_filters, strides=(2, 2), kernel_size=2, padding='same')(x)
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(num_filters, kernel_size=2, padding='same')(x)
     x = Concatenate()([x, x_1])
     x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
     out = Conv2D(num_classes, kernel_size=1, padding='same', activation="softmax")(x)
