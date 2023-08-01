bb7f477e2b2e5fdd419d5d9290eb93443312324e
Attila <attila.simko@umu.se>
2023-08-01 13:10:54 +0200

    gpu 2

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss cross_entropy --learning_rate 0.00001
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss dice --learning_rate 0.00001
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss mime --learning_rate 0.00001
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss cross_entropy --learning_rate 0.00001
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss dice --learning_rate 0.00001
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss mime --learning_rate 0.00001
 
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss cross_entropy --learning_rate 0.00005
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss dice --learning_rate 0.00005
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss mime --learning_rate 0.00005
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss cross_entropy --learning_rate 0.00005
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss dice --learning_rate 0.00005
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss mime --learning_rate 0.00005
 
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss cross_entropy --learning_rate 0.0001
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss dice --learning_rate 0.0001
-python3 training.py --gpu 0 --base gauss --num_epochs 75 --loss mime --learning_rate 0.0001
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss cross_entropy --learning_rate 0.0001
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss dice --learning_rate 0.0001
+python3 training.py --gpu 2 --base gauss --num_epochs 75 --loss mime --learning_rate 0.0001
