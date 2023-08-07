8750220fdca889ac57926d9c5af29243ff7555c2
Attila <attila.simko@umu.se>
2023-08-07 10:43:02 +0200

    go

--- script.sh
+++ script.sh
@@ -2,9 +2,7 @@
 python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 1 --beta 1
 python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0
 python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 0 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 2 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 2 --beta 0
-python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 0 --beta 2
-python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 10 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 10 --beta 0
-python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 0 --beta 10
+python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0.5
+python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 0.5 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0.1
+python3 training.py --gpu 2 --base gauss --num_epochs 150 --loss mime --learning_rate 0.00001 --alpha 0.1 --beta 1
