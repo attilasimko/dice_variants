bf1c2673e90a2da3220ccbeb02d4dccbefa1cb23
Attila <attila.simko@umu.se>
2023-08-08 13:43:55 +0200

    new model

--- model.py
+++ model.py
@@ -86,76 +86,76 @@ def decoding_block(prev_layer_input, skip_layer_input, n_filters=32):
                 kernel_initializer='HeNormal')(conv)
     return conv
 
-def unet_2d(input_shape, num_filters, num_classes, batchnorm=False):
+def unet_2d(input_shape, num_filters, num_classes, batchnorm=True):
     inp = Input(shape=input_shape)
-    x = Conv2D(num_filters, kernel_size=3, padding='same')(inp)
-    x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+    x = Conv2D(num_filters, kernel_size=3, padding='same', kernel_initializer='HeNormal')(inp)
+    x = Conv2D(num_filters, kernel_size=3, padding='same', kernel_initializer='HeNormal')(x)
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
     x_1 = x
 
-    x = Conv2D(num_filters * 2, strides=(2, 2), kernel_size=3, padding='same')(x)
+    x = Conv2D(num_filters * 2, strides=(2, 2), kernel_size=3, padding='same', kernel_initializer='HeNormal')(x)
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
     x_2 = x
 
-    x = Conv2D(num_filters * 4, strides=(2, 2), kernel_size=3, padding='same')(x)
+    x = Conv2D(num_filters * 4, strides=(2, 2), kernel_size=3, padding='same', kernel_initializer='HeNormal')(x)
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
     x_3 = x
 
-    x = Conv2D(num_filters * 8, strides=(2, 2), kernel_size=3, padding='same')(x)
+    x = Conv2D(num_filters * 8, strides=(2, 2), kernel_size=3, padding='same', kernel_initializer='HeNormal')(x)
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
     x_4 = x
 
-    x = Conv2D(num_filters * 8, strides=(2, 2), kernel_size=3, padding='same')(x)
+    x = Conv2D(num_filters * 8, strides=(2, 2), kernel_size=3, padding='same', kernel_initializer='HeNormal')(x)
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
     x_5 = x
 
-    x = Conv2D(num_filters * 8, strides=(2, 2), kernel_size=3, padding='same')(x)
-    x = Conv2D(num_filters * 8, kernel_size=3, padding='same')(x)
+    x = Conv2D(num_filters * 8, strides=(2, 2), kernel_size=3, padding='same', kernel_initializer='HeNormal')(x)
+    x = Conv2D(num_filters * 8, kernel_size=3, padding='same', kernel_initializer='HeNormal')(x)
     if (batchnorm):
         x = BatchNormalization()(x)
 
     x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(num_filters * 8, kernel_size=2, padding='same')(x)
+    x = Conv2D(num_filters * 8, kernel_size=2, padding='same', kernel_initializer='HeNormal')(x)
     x = Concatenate()([x, x_5])
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
 
     x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(num_filters * 8, kernel_size=2, padding='same')(x)
+    x = Conv2D(num_filters * 8, kernel_size=2, padding='same', kernel_initializer='HeNormal')(x)
     x = Concatenate()([x, x_4])
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
 
     x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(num_filters * 4, kernel_size=2, padding='same')(x)
+    x = Conv2D(num_filters * 4, kernel_size=2, padding='same', kernel_initializer='HeNormal')(x)
     x = Concatenate()([x, x_3])
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
 
     x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(num_filters * 2, kernel_size=2, padding='same')(x)
+    x = Conv2D(num_filters * 2, kernel_size=2, padding='same', kernel_initializer='HeNormal')(x)
     x = Concatenate()([x, x_2])
     if (batchnorm):
         x = BatchNormalization()(x)
     x = ReLU()(x)
 
     x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(num_filters, kernel_size=2, padding='same')(x)
+    x = Conv2D(num_filters, kernel_size=2, padding='same', kernel_initializer='HeNormal')(x)
     x = Concatenate()([x, x_1])
-    x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
-    out = Conv2D(num_classes, kernel_size=1, padding='same', activation="softmax")(x)
+    x = Conv2D(num_filters, kernel_size=3, padding='same', kernel_initializer='HeNormal')(x)
+    out = Conv2D(num_classes, kernel_size=1, padding='same', kernel_initializer='HeNormal', activation="softmax")(x)
 
     return Model(inp, out)
