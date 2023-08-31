24c08789f80add5e22a817c32628b0fdf647e2b1
Attila <attila.simko@umu.se>
2023-08-31 10:40:37 +0200

    oops

--- utils.py
+++ utils.py
@@ -7,7 +7,6 @@ def set_seeds(seed=42):
     os.environ['PYTHONHASHSEED'] = str(seed)
     tf.random.set_seed(seed)
     np.random.seed(seed)
-    set_seeds(seed=seed)
     
     os.environ['TF_DETERMINISTIC_OPS'] = '1'
     os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
