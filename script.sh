bb9e7ca7d92cea7fb4a84895a0dc1b4105b12fd0
Attila <attila.simko@umu.se>
2023-09-01 14:10:00 +0200

    go big

--- script.sh
+++ script.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss cross_entropy --learning_rate 0.00001
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss cross_entropy --learning_rate 0.00001
-
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss mime --learning_rate 0.00001 --alpha 1 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0.01
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0.0001
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss mime --learning_rate 0.00001 --alpha 0.01 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss mime --learning_rate 0.00001 --alpha 0.0001 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss mime --learning_rate 0.00001 --alpha 0 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss cross_entropy --learning_rate 0.00001
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss dice --learning_rate 0.00001 --alpha 1 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss dice --learning_rate 0.00001 --alpha 1 --beta 0.01
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss dice --learning_rate 0.00001 --alpha 1 --beta 0.0001
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss dice --learning_rate 0.00001 --alpha 0.01 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss dice --learning_rate 0.00001 --alpha 0.0001 --beta 1
-python3 training.py --gpu 2 --base gauss --num_epochs 25 --loss dice --learning_rate 0.00001 --alpha 0 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 1 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0.1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0.01
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0.001
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0.0001
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 1 --beta 0
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 0.1 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 0.01 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 0.001 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 0.0001 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss mime --learning_rate 0.00001 --alpha 0 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss cross_entropy --learning_rate 0.00001
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 1 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 1 --beta 0.1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 1 --beta 0.01
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 1 --beta 0.001
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 1 --beta 0.0001
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 0.1 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 0.01 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 0.001 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 0.0001 --beta 1
+python3 training.py --gpu 2 --base gauss --num_epochs 200 --loss dice --learning_rate 0.00001 --alpha 0 --beta 1
