e79660b085b56738c198a5caed1a897918001128
Attila <attila.simko@umu.se>
2023-09-01 08:18:06 +0200

    new set seeds

--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ def set_seeds(seed=42):
     tf.random.set_seed(seed)
     np.random.seed(seed)
     
-    # os.environ['TF_DETERMINISTIC_OPS'] = '1'
-    # os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
-    # tf.config.threading.set_inter_op_parallelism_threads(1)
-    # tf.config.threading.set_intra_op_parallelism_threads(1)
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(1)
 
 def compile(model, optimizer_str, lr_str, loss_str, alpha=1, beta=1, num_voxels=1):
     import tensorflow
