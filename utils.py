5809a2d13b5eca920b4e1271fa8042b402dc406f
Attila <attila.simko@umu.se>
2023-09-01 11:20:31 +0200

    maybe now?

--- utils.py
+++ utils.py
@@ -8,8 +8,8 @@ def set_seeds(seed=42):
     tf.random.set_seed(seed)
     np.random.seed(seed)
     
-    os.environ['TF_DETERMINISTIC_OPS'] = '1'
-    # os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+    # os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
     tf.config.threading.set_inter_op_parallelism_threads(1)
     tf.config.threading.set_intra_op_parallelism_threads(1)
 
