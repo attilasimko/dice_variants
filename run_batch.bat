66f88b02bd50708dfffa65463c367a0e167d0379
Attila <attila.simko@umu.se>
2023-09-11 07:40:36 +0200

    big loop

--- run_batch.bat
+++ run_batch.bat
@@ -8,23 +8,23 @@ do
 # sbatch script.sh  "cross_entropy" "$lr" 0 0 "True" "False"
 sbatch script.sh  "mime" "$lr" "0" "0" "0" "0" "0" "0" "True"
 
-for alpha1 in 0 1e6
-do
-for alpha2 in 0 1e6
-do
-for alpha3 in 0 1e6
-do 
-for beta1 in 0 1e-4
-do
-for beta2 in 0 1e-4
-do
-for beta3 in 0 1e-4
-do
+# for alpha1 in 0 1e6
+# do
+# for alpha2 in 0 1e6
+# do
+# for alpha3 in 0 1e6
+# do 
+# for beta1 in 0 1e-4
+# do
+# for beta2 in 0 1e-4
+# do
+# for beta3 in 0 1e-4
+# do
 # sbatch script.sh  "mime" "$lr" "$alpha1" "$alpha2" "$alpha3" "$beta1" "$beta2" "$beta3" "False"
-done
-done
-done
-done
-done
-done
+# done
+# done
+# done
+# done
+# done
+# done
 done
