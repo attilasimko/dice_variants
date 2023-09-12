af684d768dcdc7d58e813deb6e597999e9bacac4
Attila <attila.simko@umu.se>
2023-09-12 13:28:54 +0200

    tf disable

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ def set_seeds(seed=42):
     
     os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
     os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_DISABLE_SEGMENT_REDUCTION_OP_DETERMINISM_EXCEPTIONS '] = 1
     tf.config.threading.set_inter_op_parallelism_threads(1)
     tf.config.threading.set_intra_op_parallelism_threads(1)
     tf.keras.utils.set_random_seed(42)
