43e5fd229548cba48fdd7a7a589ec536e1bc4a02
Attila <attila.simko@umu.se>
2023-09-12 18:31:23 +0200

    big run

--- run_batch.bat
+++ run_batch.bat
@@ -3,27 +3,26 @@
 for lr in 0.00004
 do
 # sbatch script.sh "dice" "$lr" 0 0 "0" "0" "0" "0" "0" "0"
-# sbatch script.sh "dice" "$lr" 0 0 "0" "0" "0" "0" "0" "0"
 # sbatch script.sh  "cross_entropy" "$lr" 0 0 "0" "0" "0" "0" "0" "0"
-sbatch script.sh  "mime" "$lr" "-" "-" "-" "-" "-" "-"
+# sbatch script.sh  "mime" "$lr" "-" "-" "-" "-" "-" "-"
 
-# for alpha1 in "-" 0 1e6
-# do
-# for alpha2 in "-" 0 1e6
-# do
-# for alpha3 in "-" 0 1e6
-# do 
-# for beta1 in "-" 0 1e-4
-# do
-# for beta2 in "-" 0 1e-4
-# do
-# for beta3 in "-" 0 1e-4
-# do
-# sbatch script.sh  "mime" "$lr" "$alpha1" "$alpha2" "$alpha3" "$beta1" "$beta2" "$beta3"
-# done
-# done
-# done
-# done
-# done
-# done
+for alpha1 in "-" 0 1e6
+do
+for alpha2 in "-" 0 1e6
+do
+for alpha3 in "-" 0 1e6
+do 
+for beta1 in "-" 0 1e-4
+do
+for beta2 in "-" 0 1e-4
+do
+for beta3 in "-" 0 1e-4
+do
+sbatch script.sh  "mime" "$lr" "$alpha1" "$alpha2" "$alpha3" "$beta1" "$beta2" "$beta3"
+done
+done
+done
+done
+done
+done
 done
