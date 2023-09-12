31d86cccc9d63e5bdb633343def89655d1d8c43d
Attila <attila.simko@umu.se>
2023-09-12 18:39:35 +0200

    just zeros

--- run_batch.bat
+++ run_batch.bat
@@ -6,17 +6,17 @@ do
 # sbatch script.sh  "cross_entropy" "$lr" 0 0 "0" "0" "0" "0" "0" "0"
 # sbatch script.sh  "mime" "$lr" "-" "-" "-" "-" "-" "-"
 
-for alpha1 in "-" 0 1e6
+for alpha1 in "-" 0
 do
-for alpha2 in "-" 0 1e6
+for alpha2 in "-" 0
 do
-for alpha3 in "-" 0 1e6
+for alpha3 in "-" 0
 do 
-for beta1 in "-" 0 1e-4
+for beta1 in "-" 0
 do
-for beta2 in "-" 0 1e-4
+for beta2 in "-" 0
 do
-for beta3 in "-" 0 1e-4
+for beta3 in "-" 0
 do
 sbatch script.sh  "mime" "$lr" "$alpha1" "$alpha2" "$alpha3" "$beta1" "$beta2" "$beta3"
 done
