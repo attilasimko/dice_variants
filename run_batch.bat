9a232587b2fa4301f42c8712742171ea2bc76756
Attila <attila.simko@umu.se>
2023-10-03 16:41:47 +0200

    big runs

--- run_batch.bat
+++ run_batch.bat
@@ -4,12 +4,12 @@ for skip_background in "True" "False"
 do
 for data in WMH
 do
-for lr in 0.05 0.01 0.005 0.001 0.0005 0.0001
+for lr in 0.01 0.005 0.001 0.0005 0.0001
 do
 
 sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
-# sbatch script.sh  "mime" "$lr" "-0.00016" "-0.07675" "-0.02461" "0.00016" "0" "0.00642" "0.00011" "-" "$data" "$skip_background"
+sbatch script.sh  "mime" "$lr" "-0.00098" "-0.15493" "-0.06960" "-" "0.00049" "0.00432" "0.00009" "-" "$data" "$skip_background"
 
 for alpha in "-"
 do
