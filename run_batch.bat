641d9451819f440253c30898da55323f349cc9c5
Attila <attila.simko@umu.se>
2023-10-03 11:16:04 +0200

    go again.

--- run_batch.bat
+++ run_batch.bat
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for skip_background in "True" "False"
+for skip_background in "True"
 do
 for data in WMH
 do
@@ -11,15 +11,15 @@ sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_bac
 # sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 # sbatch script.sh  "mime" "$lr" "-0.00016" "-0.07675" "-0.02461" "0.00016" "0" "0.00642" "0.00011" "-" "$data" "$skip_background"
 
-# for alpha in "-"
-# do
-# for beta in "-"
-# do
+for alpha in "-"
+do
+for beta in "-"
+do
 
-# sbatch script.sh "mime" "$lr" "$alpha" "$alpha" "$alpha" "-" "$beta" "$beta" "$beta" "-" "$data" "$skip_background"
+sbatch script.sh "mime" "$lr" "$alpha" "$alpha" "$alpha" "-" "$beta" "$beta" "$beta" "-" "$data" "$skip_background"
 
-# done
-# done
+done
+done
 
 done
 done
