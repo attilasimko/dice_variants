5daf62bd942fda22f6361142d9c0f89473ca6100
Attila <attila.simko@umu.se>
2023-10-03 16:30:59 +0200

    longer runs

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2023-5-108 -p alvis
 #SBATCH -N 1 --gpus-per-node=A100:1
-#SBATCH --time=00-03:00:00
+#SBATCH --time=01-00:00:00
 #SBATCH --error=/cephyr/users/attilas/Alvis/out/%J_error.out
 #SBATCH --output=/cephyr/users/attilas/Alvis/out/%J_output.out
 
@@ -20,5 +20,5 @@ export var9=$9
 export var10=${10}
 export var11=${11}
 export var12=${12}
-python3 training.py --base alvis --skip_background $var12 --batch_size 12 --dataset $var11 --num_epochs 5 --loss $var1 --learning_rate $var2 --alpha1 $var3 --alpha2 $var4 --alpha3 $var5 --alpha4 $var6 --beta1 $var7 --beta2 $var8 --beta3 $var9 --beta4 $var10
+python3 training.py --base alvis --skip_background $var12 --batch_size 12 --dataset $var11 --num_epochs 50 --loss $var1 --learning_rate $var2 --alpha1 $var3 --alpha2 $var4 --alpha3 $var5 --alpha4 $var6 --beta1 $var7 --beta2 $var8 --beta3 $var9 --beta4 $var10
 wait
