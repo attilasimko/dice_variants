4844da8c405957bb06c8ef7adc8321c0644b8d8c
Attila <attila.simko@umu.se>
2023-10-03 08:19:16 +0200

    Argh, let's plot again.

--- run_batch.bat
+++ run_batch.bat
@@ -4,22 +4,22 @@ for skip_background in "True" "False"
 do
 for data in WMH
 do
-for lr in 0.05 0.01 0.005 0.001 0.0005 0.0001
+for lr in 0.005
 do
 
 sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
-sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
+# sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 # sbatch script.sh  "mime" "$lr" "-0.00016" "-0.07675" "-0.02461" "0.00016" "0" "0.00642" "0.00011" "-" "$data" "$skip_background"
 
-for alpha in "-"
-do
-for beta in "-"
-do
+# for alpha in "-"
+# do
+# for beta in "-"
+# do
 
-sbatch script.sh "mime" "$lr" "$alpha" "$alpha" "$alpha" "-" "$beta" "$beta" "$beta" "-" "$data" "$skip_background"
+# sbatch script.sh "mime" "$lr" "$alpha" "$alpha" "$alpha" "-" "$beta" "$beta" "$beta" "-" "$data" "$skip_background"
 
-done
-done
+# done
+# done
 
 done
 done
