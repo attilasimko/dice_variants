5713020a5eb38d4110c7a4b5c9cc5be9676c03bd
Attila <attila.simko@umu.se>
2023-10-05 14:12:46 +0200

    big time.

--- run_batch.bat
+++ run_batch.bat
@@ -4,19 +4,19 @@ for skip_background in "True"
 do
 for data in WMH
 do
-for lr in 0.0005
+for lr in 0.01 0.001 0.0001
 do
 
 sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 
-for alpha2 in "rand"
+for alpha2 in "rand" "rand" "rand" "rand"
 do 
-for beta2 in "rand"
+for beta2 in "rand" "rand" "rand" "rand"
 do
-for alpha3 in "rand"
+for alpha3 in "rand" "rand" "rand" "rand"
 do 
-for beta3 in "rand"
+for beta3 in "rand" "rand" "rand" "rand"
 do
 
 sbatch script.sh "coin" "$lr" "-" "$alpha2" "$alpha3" "-" "-" "$beta2" "$beta3" "-" "$data" "$skip_background"
