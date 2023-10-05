cb59e808ae091eaf1312c5a8201c97b03c5e99e5
Attila <attila.simko@umu.se>
2023-10-05 19:07:38 +0200

    big batch.

--- run_batch.bat
+++ run_batch.bat
@@ -10,13 +10,13 @@ do
 sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 
-for alpha2 in "rand" "rand" "rand" "rand"
+for alpha2 in "-1.0" "-0.8" "-0.6" "-0.8" "-0.4" "-0.2" "0"
 do 
-for beta2 in "rand" "rand" "rand" "rand"
+for beta2 in "1.0" "0.8" "0.6" "0.8" "0.4" "0.2" "0"
 do
-for alpha3 in "rand" "rand" "rand" "rand"
+for alpha3 in "-1.0" "-0.8" "-0.6" "-0.8" "-0.4" "-0.2" "0"
 do 
-for beta3 in "rand" "rand" "rand" "rand"
+for beta3 in "1.0" "0.8" "0.6" "0.8" "0.4" "0.2" "0"
 do
 
 sbatch script.sh "coin" "$lr" "-" "$alpha2" "$alpha3" "-" "-" "$beta2" "$beta3" "-" "$data" "$skip_background"
