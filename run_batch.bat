fda83a953549ac16f1615d379df5f987eeec2de9
Attila <attila.simko@umu.se>
2023-10-06 09:31:10 +0200

    new batch

--- run_batch.bat
+++ run_batch.bat
@@ -10,16 +10,20 @@ do
 sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 
-for alpha2 in "-1.0" "-0.6" "-0.3" "0"
-do 
-for beta2 in "1.0" "0.6" "0.3" "0"
+for alpha1 in "-" "0"
 do
-for alpha3 in "-1.0" "-0.6" "-0.3" "0"
+for beta1 in "-" "0"
+do
+for alpha2 in "-" "0"
 do 
-for beta3 in "1.0" "0.6" "0.3" "0"
+for beta2 in "-" "0"
+do
+for alpha3 in "-" "0"
+do 
+for beta3 in "-" "0"
 do
 
-sbatch script.sh "coin" "$lr" "-" "$alpha2" "$alpha3" "-" "-" "$beta2" "$beta3" "-" "$data" "$skip_background"
+sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "-" "$beta1" "$beta2" "$beta3" "-" "$data" "$skip_background"
 
 done
 done
