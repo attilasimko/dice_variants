4f34b096744bd7192352cb87a6eaf8ffacc5098b
Attila <attila.simko@umu.se>
2023-10-18 07:35:02 +0200

    uncomments#

--- run_batch.bat
+++ run_batch.bat
@@ -8,7 +8,7 @@ for lr in 0.001
 do
 
 sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
-# sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
+sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background"
 
 for alpha1 in "-" "0" "-0.0000153"
 do
@@ -23,8 +23,7 @@ do
 for beta3 in "-" "0" "0.0024809"
 do
 
-echo "hi"
-# sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "-" "$beta1" "$beta2" "$beta3" "-" "$data" "$skip_background"
+sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "-" "$beta1" "$beta2" "$beta3" "-" "$data" "$skip_background"
 
 done
 done
