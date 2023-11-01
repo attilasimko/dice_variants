61ba0ca8ef4eb0628caa74522295cbc6b214fe2e
Attila <attila.simko@umu.se>
2023-11-01 06:37:33 +0100

    oops# Please enter the commit message for your changes. Lines starting

--- run_batch.bat
+++ run_batch.bat
@@ -6,35 +6,35 @@ for data in WMH
 do
 for lr in 0.001
 do
-for dskip in 0 1 2 3 4 5
+for dskip in 0
 do
 
-sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background" "$dskip"
-sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background" "$dskip"
+# sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background" "$dskip"
+# sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background" "$dskip"
 # sbatch script.sh "squared_dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$skip_background" "$dskip"
 
 
-# for alpha1 in "boundary"
-# do
-# for beta1 in "0"
-# do
-# for alpha2 in "boundary"
-# do 
-# for beta2 in "0"
-# do
-# for alpha3 in "boundary"
-# do 
-# for beta3 in "0"
-# do
+for alpha1 in "boundary"
+do
+for beta1 in "0"
+do
+for alpha2 in "boundary"
+do 
+for beta2 in "0"
+do
+for alpha3 in "boundary"
+do 
+for beta3 in "0"
+do
 
-# sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "-" "$beta1" "$beta2" "$beta3" "-" "$data" "$skip_background" "$dskip"
+sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "-" "$beta1" "$beta2" "$beta3" "-" "$data" "$skip_background" "$dskip"
 
-# done
-# done
-# done
-# done
-# done
-# done
+done
+done
+done
+done
+done
+done
 
 done
 done
