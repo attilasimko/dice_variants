7c44e3f39317261d2f1fde2c0cacd7d0b0ecaafc
Attila <attila.simko@umu.se>
2023-11-02 15:35:17 +0100

    new mean

--- utils.py
+++ utils.py
@@ -157,7 +157,7 @@ def coin_loss(_alphas, _betas, epsilon):
                 beta = float(betas[i])
 
             loss.append(1 + K.sum(alpha * y_true[:, :, :, i] * y_pred[:, :, :, i] + beta * y_pred[:, :, :, i]))
-        return K.mean(K._to_tensor(loss, tf.float64))
+        return tf.reduce_mean(tf.constant(loss))
     return loss_fn
 
 def plot_grad(x, y, model, idx):
