44d464d17faf7f85cf54938dbc9b65d5e0824b04
Attila <attila.simko@umu.se>
2023-11-06 17:42:01 +0100

    alpha and beta in sync

--- utils.py
+++ utils.py
@@ -163,7 +163,7 @@ def coin_loss(_alphas, _betas, epsilon):
             if (replace_betas[i]):
                 beta = tf.stop_gradient(coin_coef_b(y_true[:, :, :, i], y_pred[:, :, :, i], epsilon))
             else:
-                beta = np.random.uniform(0.0, 2.0) # float(betas[i])
+                beta = np.random.uniform(0.0, 0.02) # float(betas[i])
 
             loss += (1 + K.sum(alpha * y_true[:, :, :, i] * y_pred[:, :, :, i] + beta * y_pred[:, :, :, i]))
             iter += 1
