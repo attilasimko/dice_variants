95cd3b94387ae2752940e762b33c8b200afde83c
Attila <attila.simko@umu.se>
2023-11-06 11:13:05 +0100

    min instead

--- utils.py
+++ utils.py
@@ -327,4 +327,4 @@ def evaluate(experiment, gen, model, name, labels, epoch):
     plt.close()
     experiment.log_image(save_path + "coefs.png", step=epoch)
 
-    return (np.reshape(np.hstack(grads).T, (-1)), [coin_a(np.mean(u)) for u in metric_u], [coin_b(np.mean(u), np.mean(i)) for (u, i) in zip(metric_u, metric_i)])
+    return (np.reshape(np.hstack(grads).T, (-1)), [coin_a(np.min(u)) for u in metric_u], [coin_b(np.min(u), np.min(i)) for (u, i) in zip(metric_u, metric_i)])
