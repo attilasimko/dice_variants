1a3b07b0ced5be65f5f69097341f7a45e1fdb834
Attila <attila.simko@umu.se>
2023-11-08 16:28:02 +0100

    evaluate fix

--- utils.py
+++ utils.py
@@ -265,11 +265,11 @@ def evaluate(experiment, gen, model, name, labels, epoch):
             
             if (name != "train"):
                 grad = []
-                # for i in range(np.shape(current_y)[0]):
+                for i in range(np.shape(current_y)[0]):
+                    grad.append([coin_I(current_y[i, :, :], current_pred[i, :, :]).numpy(),
+                                coin_U(current_y[i, :, :], current_pred[i, :, :]).numpy()])
                 metric_dice_a[j].append(coin_coef_a(current_y[:, :, :], current_pred[:, :, :]).numpy())
                 metric_dice_b[j].append(coin_coef_b(current_y[:, :, :], current_pred[:, :, :]).numpy())
-                grad.append([coin_I(current_y[:, :, :], current_pred[:, :, :]).numpy(),
-                            coin_U(current_y[:, :, :], current_pred[:, :, :]).numpy()])
                 metric_u[j].append(coin_U(current_y[:, :, :], current_pred[:, :, :]).numpy())
                 metric_i[j].append(coin_I(current_y[:, :, :], current_pred[:, :, :]).numpy())
                 
