6dc54c98a40dd1a9c22ab3f560a235e385c524bf
Attila <attila.simko@umu.se>
2023-11-23 17:28:15 +0100

    coin batch# Please enter the commit message for your changes. Lines starting

--- run_batch.sh
+++ run_batch.sh
@@ -9,38 +9,38 @@ do
 for dskip in 0
 do
 
-sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$dskip"
+# sbatch script.sh  "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$dskip"
 # sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$dskip"
 # sbatch script.sh "squared_dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$dskip"
 
 
-# for alpha1 in "-"
-# do
-# for beta1 in "-"
-# do
-# for alpha2 in "-"
-# do 
-# for beta2 in "-"
-# do
-# for alpha3 in "-"
-# do 
-# for beta3 in "-"
-# do
-# for alpha4 in "-"
-# do 
-# for beta4 in "-"
-# do
+for alpha1 in "-"
+do
+for beta1 in "-"
+do
+for alpha2 in "-"
+do 
+for beta2 in "-"
+do
+for alpha3 in "-"
+do 
+for beta3 in "-"
+do
+for alpha4 in "-"
+do 
+for beta4 in "-"
+do
 
-# sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "$beta1" "$beta2" "$beta3" "$beta4" "$data" "$dskip"
+sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "$beta1" "$beta2" "$beta3" "$beta4" "$data" "$dskip"
 
-# done
-# done
-# done
-# done
-# done
-# done
-# done
-# done
+done
+done
+done
+done
+done
+done
+done
+done
 
 done
 done
