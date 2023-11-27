69359f0dd01762274665903cd9ce9526cbd6297f
Attila <attila.simko@umu.se>
2023-11-27 11:17:45 +0100

    b only y_pred

--- utils.py
+++ utils.py
@@ -171,8 +171,8 @@ def coin_loss(_alphas, _betas, epsilon):
                     # beta = tf.stop_gradient(coin_coef_b(y_true[slc, :, :, i], y_pred[slc, :, :, i], epsilon))
                     y_true_f = K.flatten(y_true[slc, :, :, i])
                     y_pred_f = tf.stop_gradient(K.flatten(y_pred[slc, :, :, i]))
-                    I = K.sum(y_true_f * y_pred_f)
-                    U = K.sum(y_true_f) + K.sum(y_pred_f) + epsilon
+                    I = K.sum(y_pred_f * y_pred_f)
+                    U = K.sum(y_pred_f) + K.sum(y_pred_f) + epsilon
                     beta = tf.stop_gradient(2 * I / U**2)
                 else:
                     beta = float(betas[i])
