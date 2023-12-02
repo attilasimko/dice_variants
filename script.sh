f5a9fc7eb548a4043fe6ddd34860e620b1c48a5d
Attila <attila.simko@umu.se>
2023-12-02 15:22:43 +0100

    no supervision for background

--- script.sh
+++ script.sh
@@ -20,5 +20,5 @@ export var9=$9
 export var10=${10}
 export var11=${11}
 export var12=${12}
-python3 training.py --base alvis --optimizer "Adam" --skip_background "False" --batch_size 12 --dataset $var11 --num_epochs 200 --loss $var1 --learning_rate $var2 --alpha1 $var3 --alpha2 $var4 --alpha3 $var5 --alpha4 $var6 --beta1 $var7 --beta2 $var8 --beta3 $var9 --beta4 $var10 --dskip $var12
+python3 training.py --base alvis --optimizer "Adam" --skip_background "True" --batch_size 12 --dataset $var11 --num_epochs 200 --loss $var1 --learning_rate $var2 --alpha1 $var3 --alpha2 $var4 --alpha3 $var5 --alpha4 $var6 --beta1 $var7 --beta2 $var8 --beta3 $var9 --beta4 $var10 --dskip $var12
 wait
