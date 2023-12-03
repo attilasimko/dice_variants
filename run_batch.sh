41b1b8fcf0d4b117c70b6b7dd413511edb6df4d5
Attila <attila.simko@umu.se>
2023-12-03 15:22:38 +0100

    dice

--- run_batch.sh
+++ run_batch.sh
@@ -9,40 +9,40 @@ do
 for dskip in 0
 do
 
-# sbatch script.sh "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$dskip"
-# sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$dskip"
+sbatch script.sh "cross_entropy" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$dskip"
+sbatch script.sh "dice" "$lr" "-" "-" "-" "-" "-" "-" "-" "-" "$data" "$dskip"
 
 
-for alpha1 in "-"
-do
-for beta1 in "-"
-do
-for alpha2 in "-"
-do 
-for beta2 in "-"
-do
-for alpha3 in "-"
-do 
-for beta3 in "-"
-do
-for alpha4 in "-"
-do 
-for beta4 in "-"
-do
+# for alpha1 in "-"
+# do
+# for beta1 in "-"
+# do
+# for alpha2 in "-"
+# do 
+# for beta2 in "-"
+# do
+# for alpha3 in "-"
+# do 
+# for beta3 in "-"
+# do
+# for alpha4 in "-"
+# do 
+# for beta4 in "-"
+# do
 
-sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "$beta1" "$beta2" "$beta3" "$beta4" "$data" "$dskip"
-# sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "1" "1" "1" "1" "$data" "$dskip"
-# sbatch script.sh "coin" "$lr" "1" "1" "1" "1" "$beta1" "$beta2" "$beta3" "$beta4" "$data" "$dskip"
-# sbatch script.sh "coin" "$lr" "1" "1" "1" "1" "0.1" "0.1" "0.1" "0.1" "$data" "$dskip"
+# sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "$beta1" "$beta2" "$beta3" "$beta4" "$data" "$dskip"
+# # sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "1" "1" "1" "1" "$data" "$dskip"
+# # sbatch script.sh "coin" "$lr" "1" "1" "1" "1" "$beta1" "$beta2" "$beta3" "$beta4" "$data" "$dskip"
+# # sbatch script.sh "coin" "$lr" "1" "1" "1" "1" "0.1" "0.1" "0.1" "0.1" "$data" "$dskip"
 
-done
-done
-done
-done
-done
-done
-done
-done
+# done
+# done
+# done
+# done
+# done
+# done
+# done
+# done
 
 done
 done
