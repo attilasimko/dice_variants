6ce4da31c959cf2692bf4649c8c70ef74315ed5a
Attila <attila.simko@umu.se>
2023-12-03 17:52:56 +0100

    now...

--- utils.py
+++ utils.py
@@ -173,10 +173,10 @@ def coin_loss(_alphas, _betas, epsilon):
 
                 if (replace_betas[i]):
                     # beta = tf.stop_gradient(coin_coef_b(y_true[slc, :, :, i], y_pred[slc, :, :, i], epsilon))
-                    beta = tf.cast(tf.stop_gradient(2 * I / U), tf.float64) # 2 * I / U
+                    beta = tf.cast(tf.stop_gradient(2 * I / (U * U)), tf.float64) # 2 * I / U
                 else:
                     beta = float(betas[i])
-                loss += K.sum(((- 2 * alpha) * y_true[slc, :, :, i] * y_pred[slc, :, :, i]) + ((beta * alpha) * y_pred[slc, :, :, i]))
+                loss += K.sum(((- alpha) * y_true[slc, :, :, i] * y_pred[slc, :, :, i]) + ((alpha) * y_pred[slc, :, :, i]))
         return loss / y_true.shape[0]
     return loss_fn
 
