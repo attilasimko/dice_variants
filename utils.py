77b90f2cb16ece664fd4c2ad4cc32fd750f576b0
Attila <attila.simko@umu.se>
2023-12-07 15:06:30 +0100

    with bacgkround

--- utils.py
+++ utils.py
@@ -170,19 +170,19 @@ def coin_loss(_alphas, _betas, epsilon):
                 if (replace_alphas[i]):
                     # alpha = tf.stop_gradient(coin_coef_a(y_true[slc, :, :, i], y_pred[slc, :, :, i], epsilon))
                     # tf.cast(tf.where(tf.not_equals(tf.argmax(y_pred[slc, :, :, :], -1), i), tf.float64) * 
-                    alpha = tf.stop_gradient(tf.cast(2 / (val_mean), tf.float64)) # 2 / U
+                    alpha = tf.stop_gradient(tf.cast(2.0, tf.float64)) # 2 / U # 2 / (val_mean)
                 else:
                     alpha = float(alphas[i])
 
                 if (replace_betas[i]):
                     # beta = tf.stop_gradient(coin_coef_b(y_true[slc, :, :, i], y_pred[slc, :, :, i], epsilon))
-                    beta = tf.stop_gradient(tf.cast(0.0, tf.float64)) # 2 * I / (U * U)
+                    beta = tf.stop_gradient(tf.cast(0.1, tf.float64)) # 2 * I / (U * U)
                 else:
                     beta = float(betas[i])
 
                 if (alpha < beta):
                     raise ValueError("Positive gradient overflow. Alpha < Beta")
-                loss += K.sum((- alpha * y_true[slc, :, :, i] * y_pred[slc, :, :, i]) + (beta * y_pred[slc, :, :, i]))
+                loss += K.mean((- alpha * y_true[slc, :, :, i] * y_pred[slc, :, :, i]) + (beta * y_pred[slc, :, :, i]))
         return loss / y_true.shape[0]
     return loss_fn
 
