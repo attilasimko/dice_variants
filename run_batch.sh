993912c165174e85063fb152e3beb5f68a0b1f3f
Attila <attila.simko@umu.se>
2023-12-13 21:47:42 +0100

    go

--- run_batch.sh
+++ run_batch.sh
@@ -30,7 +30,7 @@ do
 for beta4 in "-"
 do
 
-sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "0" "0" "0" "0" "$data" "$dskip"
+sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "$beta1" "$beta2" "$beta3" "$beta4" "$data" "$dskip"
 # sbatch script.sh "coin" "$lr" "$alpha1" "$alpha2" "$alpha3" "$alpha4" "1" "1" "1" "1" "$data" "$dskip"
 # sbatch script.sh "coin" "$lr" "1" "1" "1" "1" "$beta1" "$beta2" "$beta3" "$beta4" "$data" "$dskip"
 # sbatch script.sh "coin" "$lr" "1" "1" "1" "1" "0.1" "0.1" "0.1" "0.1" "$data" "$dskip"
