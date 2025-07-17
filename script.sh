66cc65bc14e4b6dae8764290e0fbc57d1bc31d40
attilasimko <attila.simko@umu.se>
2025-07-17 23:22:55 +0200

    15

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2025-5-103 -p alvis
 #SBATCH -N 1 --gpus-per-node=A40:1
-#SBATCH --time=00-01:00:00
+#SBATCH --time=00-15:00:00
 #SBATCH --mail-user=attila.simko@umu.se --mail-type=end
 #SBATCH --error=/cephyr/users/attilas/Alvis/out/%J_error.out
 #SBATCH --output=/cephyr/users/attilas/Alvis/out/%J_output.out
@@ -21,4 +21,4 @@ export var8=$8
 export var9=$9
 export var10=${10}
 export var11=${11}
-python3 training.py --base alvis --optimizer "SGD" --skip_background "False" --batch_size 12 --dataset $var11 --num_epochs 200 --loss $var1 --learning_rate $var2 --alpha1 $var3 --alpha2 $var4 --alpha3 $var5 --alpha4 $var6 --beta1 $var7 --beta2 $var8 --beta3 $var9 --beta4 $var10
+python3 training.py --base alvis --optimizer "SGD" --skip_background "False" --batch_size 8 --dataset $var11 --num_epochs 200 --loss $var1 --learning_rate $var2 --alpha1 $var3 --alpha2 $var4 --alpha3 $var5 --alpha4 $var6 --beta1 $var7 --beta2 $var8 --beta3 $var9 --beta4 $var10
