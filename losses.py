0b11261fb7842cc8ab05eb3d7e9b7109b67d549b
attilasimko <attila.simko@umu.se>
2025-08-12 16:59:41 +0200

    2hr runs

--- losses.py
+++ losses.py
@@ -3,6 +3,18 @@ from tensorflow.python.keras import backend as K
 import tensorflow as tf
 import numpy as np
 
+def _maybe_softmax(y_pred, from_logits):
+    return tf.nn.softmax(y_pred, axis=-1) if from_logits else y_pred
+
+
+def _weights_present_classes(y_true):
+    # (B,H,W,C) one-hot -> weight 1/num_present for present classes, else 0
+    present = tf.reduce_sum(y_true, axis=[1, 2]) > 0            # (B,C) bool
+    present = tf.cast(present, y_true.dtype)
+    denom = tf.reduce_sum(present, axis=-1, keepdims=True)      # (B,1)
+    denom = tf.maximum(denom, 1.0)
+    return present / denom                                      # (B,C)
+
 def get_coeffs(y_true, y_pred):
     return coin_I(y_true, y_pred).numpy(), coin_U(y_true, y_pred).numpy(), coin_coef_a(y_true, y_pred).numpy(), coin_coef_b(y_true, y_pred).numpy()
     
@@ -30,7 +42,7 @@ def coin_U(y, s, epsilon=1):
     return (K.sum(y) + K.sum(s)) + epsilon
 
 def coin_U_squared(y, s, epsilon=1):
-    return (K.sum(y)**2 + K.sum(s)**2) + epsilon
+    return (K.sum(y**2) + K.sum(s**2)) + epsilon
 
 def coin_I(y, s):
     return K.sum(y * s)
@@ -42,23 +54,6 @@ def dice_coef(y_true, y_pred, epsilon=1):
     union = tf.expand_dims(coin_U(y_true_f, y_pred_f, epsilon), 0)
     return 2. * intersection / union
 
-def squared_dice_coef(y_true, y_pred, epsilon=1):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = tf.expand_dims(coin_I(y_true_f, y_pred_f), 0)
-    union = tf.expand_dims(coin_U_squared(y_true_f, y_pred_f, epsilon), 0)
-    return 2. * intersection / union
-
-def cross_entropy_loss():
-    def loss_fn(y_true, y_pred):
-        loss = 0.0
-        iter = 0
-        for slc in range(y_true.shape[0]):
-            loss += tf.losses.categorical_crossentropy(y_true[slc, :, :, :], y_pred[slc, :, :, :])
-            iter += 1
-        return loss / y_true.shape[0]
-    return loss_fn
-
 def dice_loss(epsilon=1):
     def loss_fn(y_true, y_pred):
         loss = 0.0
@@ -68,15 +63,12 @@ def dice_loss(epsilon=1):
         return loss / y_true.shape[0]
     return loss_fn
 
-def dice_ce_loss(epsilon=1):
-    def loss_fn(y_true, y_pred):
-        loss = 0.0
-        for slc in range(y_true.shape[0]):
-            loss += tf.losses.categorical_crossentropy(y_true[slc, :, :, :], y_pred[slc, :, :, :])
-            for i in range(y_true.shape[3]):
-                loss += 1 - dice_coef(y_true[slc, :, :, i], y_pred[slc, :, :, i], epsilon)
-        return loss / y_true.shape[0]
-    return loss_fn
+def squared_dice_coef(y_true, y_pred, epsilon=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = tf.expand_dims(coin_I(y_true_f, y_pred_f), 0)
+    union = tf.expand_dims(coin_U_squared(y_true_f, y_pred_f, epsilon), 0)
+    return 2. * intersection / union
 
 def squared_dice_loss(epsilon=1):
     def loss_fn(y_true, y_pred):
@@ -88,6 +80,24 @@ def squared_dice_loss(epsilon=1):
                 iter += 1
         return loss / iter
     return loss_fn
+
+def cross_entropy_loss():
+    ce = tf.keras.losses.CategoricalCrossentropy(from_logits=False, reduction='none')
+    def loss_fn(y_true, y_pred):
+        y_true = tf.cast(y_true, y_pred.dtype)
+        ce_map = ce(y_true, y_pred)              # (B,H,W)
+        return K.mean(ce_map)
+    return loss_fn
+
+def dice_ce_loss(epsilon=1):
+    ce_loss = cross_entropy_loss()
+    dice_loss_fn = dice_loss(epsilon)
+    def loss_fn(y_true, y_pred):
+        return ce_loss(y_true, y_pred) + dice_loss_fn(y_true, y_pred)
+    return loss_fn
+
+def boundary_loss(y_true, y_pred):
+    raise NotImplementedError
 
 def coin_loss(_alphas, _betas, epsilon):
     import tensorflow as tf
@@ -110,13 +120,6 @@ def coin_loss(_alphas, _betas, epsilon):
             replace_betas[i] = True
 
     def loss_fn(y_true, y_pred):
-        # avg_sums = np.multiply([0.9684, 0.0106, 0.0102, 0.0108], 65536.0) # ["Background", "LV", "RV", "Myo"]
-        avg_sums = np.multiply([0.9694, 0.0102, 0.0102, 0.0102], 65536.0) # ["Background", "LV", "RV", "Myo"]
-        avg_as = [-1.5756e-5, -0.00144, -0.00150, -0.00141]
-        avg_bs = [65536.0, 1.0, 1.0, 1.0]
-        # avg_sums = np.multiply([0.9997, 0.0001, 0.0001, 0.0001], 65536.0)
-        if (np.sum(avg_sums) != 65536.0):
-            raise ValueError("Sum of averages is not 1.0")
         
         loss = 0.0
         for slc in range(y_true.shape[0]):
@@ -125,36 +128,21 @@ def coin_loss(_alphas, _betas, epsilon):
                 flat_pred = tf.stop_gradient(K.flatten(y_pred[slc, :, :, i]))
                 U = K.sum(flat_true) + K.sum(flat_pred) + epsilon
                 I = K.sum(flat_true * flat_pred)
-                mask = tf.cast(tf.less(y_pred[slc, :, :, i], 0.75), tf.float64)
-                val_mean = avg_sums[i]
 
                 if (replace_alphas[i]):
                     alpha = tf.stop_gradient(tf.cast(2 / U, tf.float64))
-                    # Best so far: alpha = tf.stop_gradient(tf.cast(2 / (K.sum(flat_pred) + epsilon), tf.float64))
                 else:
                     alpha = float(alphas[i])
+                    # Best so far: alpha = tf.stop_gradient(tf.cast(2 / (K.sum(flat_pred) + epsilon), tf.float64))
 
                 if (replace_betas[i]):
                     beta = tf.stop_gradient(tf.cast(2 * I / (U * U), tf.float64))
-                    # Best so far: beta = tf.stop_gradient(tf.cast(2 / (val_mean * val_mean), tf.float64))
                 else:
                     beta = float(betas[i])
+                    # Best so far: beta = tf.stop_gradient(tf.cast(2 / (val_mean * val_mean), tf.float64))
 
-                # if (tf.reduce_any(alpha < beta)):
-                #     raise ValueError("Positive gradient overflow. Alpha < Beta")
             
                 loss += K.sum((- alpha * y_true[slc, :, :, i] * y_pred[slc, :, :, i]) + (beta * y_pred[slc, :, :, i]))
 
         return loss / y_true.shape[0]
     return loss_fn
-
-def boundary_loss(y_true, y_pred):
-    raise NotImplementedError
-
-def dice_squared_loss(y_true, y_pred, epsilon=0.1):    
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(K.square(y_true_f) + K.square(y_pred_f) + epsilon)
-    difference = K.sum(K.square(y_true_f - y_pred_f))
-    dice = ((difference / intersection) + difference / (tf.cast(tf.size(y_true_f), tf.float64)))
-    return dice
