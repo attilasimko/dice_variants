e74c3849f3a9246a349f4b746a55b9c7486a5166
attilasimko <attila.simko@umu.se>
2026-09-22 11:28:08 +0200

    new eenv

--- env.sh
+++ env.sh
@@ -7,6 +7,10 @@ VENV_PATH=${ROOT}/nnUNet_env
 export nnUNet_raw=${ROOT}/dice_variants/nnUNet_raw
 export nnUNet_preprocessed=${ROOT}/dice_variants/nnUNet_preprocessed
 export nnUNet_results=${ROOT}/dice_variants/nnUNet_results
+# sbatch passes on the submitting shell's environment; nnUNet_n_proc_DA=0 (as set for
+# the CBCT project) crashes nnU-Net's planner in torch.set_num_threads(0)
+export nnUNet_n_proc_DA=${SLURM_CPUS_PER_TASK:-8}
+export nnUNet_def_n_proc=${SLURM_CPUS_PER_TASK:-8}
 
 export PYTHONUNBUFFERED=1
 export PYTHONFAULTHANDLER=1
